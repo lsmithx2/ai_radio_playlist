51549ef8d52cf7ebfbed2210e2f4f2418f7b19dc
Alicia Grandjean <alicia.grandjean@bbc.co.uk>
2018-08-14 16:40:06 +0100

    add a config component and retrieve the 4 playlists and add them to the app.js state

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,38 @@
 import React, { Component } from 'react';
 import '../App.css';
-import playlist1 from '../playlists/radio1.json';
-// import '../playlists/radio2.json';
-// import '../playlists/radio3.json';
-// import '../playlists/radio6.json';
-import {cloneDeep} from 'lodash';
+// import { radio1, radio2, radio3, radio6 } from '../playlists/*.json';
+import radio1 from '../playlists/radio1.json';
+import radio2 from '../playlists/radio2.json';
+import radio3 from '../playlists/radio3.json';
+import radio6 from '../playlists/radio6.json';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      current:{
-        song: "",
-        playlist:0
+      playlists: [],
+      current: {
+        song: '',
+        playlist: 0
       }
-    }
+    };
   }
+
   componentWillMount() {
-    const loadData = () => cloneDeep(playlist1)
-    console.log('loadData', loadData);
+    const radios = [radio1, radio2, radio3, radio6]
+    let playlists = [];
+    radios.forEach(function(item, key) {
+      playlists.push(item);
+    });
+    this.setState({playlists : playlists});
   }
-    render() {
-    console.log("playlist", this.state.current.playlist)
+
+  render() {
+    console.log('playlist', this.state.playlists);
     return (
       <div className="App">
         <div>Introducing playlist</div>
+        add playlist component here
       </div>
     );
   }
---
+++ src/components/config.js
@@ -0,0 +1,5 @@
+const config = {
+  radios: [radio1, radio2, radio3, radio6]
+};
+
+export default config;
