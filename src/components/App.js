bfdbc743b67246574607975806858f6371b66524
Kristine Grivcova <kristine.grivcova@bbc.co.uk>
2018-08-14 14:06:13 +0100

    trying to load json

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react';
 import '../App.css';
+import playlist1 from '../playlists/radio1.json';
+// import '../playlists/radio2.json';
+// import '../playlists/radio3.json';
+// import '../playlists/radio6.json';
+import {cloneDeep} from 'lodash';
 
 class App extends Component {
-  render() {
+  constructor(){
+    super();
+    this.state = {
+      current:{
+        song: "",
+        playlist:0
+      }
+    }
+  }
+  componentWillMount() {
+    const loadData = () => cloneDeep(playlist1)
+    console.log('loadData', loadData);
+  }
+    render() {
+    console.log("playlist", this.state.current.playlist)
     return (
       <div className="App">
         <div>Introducing playlist</div>
